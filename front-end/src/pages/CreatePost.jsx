624ddac723e44f93444c50892914df6b39d87efd
Abhinav Agarwal <abhinavrox17@gmail.com>
2023-01-29 22:37:16 +0530

    fifth commit

--- front-end/src/pages/CreatePost.jsx
+++ front-end/src/pages/CreatePost.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {preview} from '../assets';
+import {getRandomPrompt} from '../utils'
+import { FormField,Loader } from '../components';
 
-export default function CreatePost() {
+const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form , setForm]= useState({
+    name:'',
+    prompt:'',
+    photo:''
+  })
+  const  [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div >
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+         Create amazing images by just giving a prompt!!! Remember imagination is the key!!
+        </p>
+      </div>
+    </section>
   )
 }
+
+export default CreatePost
