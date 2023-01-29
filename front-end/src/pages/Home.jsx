982b92d04ed992347e6048d6545d96c7dec1d4bc
Abhinav Agarwal <abhinavrox17@gmail.com>
2023-01-29 22:10:37 +0530

    forth commit

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -5,6 +5,17 @@ import { FormField,Loader } from '../components'
 const Home = () => {
 const [loading,setLoading] = useState(false)
 const[allPosts, setAllPosts]= useState(null)
+const [searchText, setsearchText] = useState()
+const RenderCards= ({data, title})=>{
+  if(data?.length>0)
+  { return data.map((post)=><Card key={post._id}{...post}/>)}
+  return (
+    <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>
+      {title}
+    </h2>
+  )
+
+}
  
 
   return (
@@ -24,7 +35,24 @@ const[allPosts, setAllPosts]= useState(null)
             <Loader/>
           </div>
         ):(
-          <></>
+          <>
+          {searchText&&(
+            <h2 className="font-medium text-[#666] text-xl mb-3">
+              Showing results for <span className="text-[#222328]">
+                {searchText}
+              </span>
+            </h2>
+          )}
+          <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+            {searchText ? (
+              <RenderCards data={[]} title="No search results found"/>
+            )
+            :
+            (
+              <RenderCards data={[]} title="No posts found"/>
+            )}
+          </div>
+          </>
         )}
       </div>
       
